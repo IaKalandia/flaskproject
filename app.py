532260f2a3954f7288d6d1ee639f65ef42c47d30
Ia Kalandia <iakalandia1@gmail.com>
2025-06-02 17:42:48 +0400

    adding modified files

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+# create a Form class 
+class NamerForm(FlaskForm):
+    name = StringField("what is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # create a route decorator (this is how it called!)
 @app.route('/')
@@ -12,7 +22,21 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('add_user.html',
+         name=name, 
+         form=form)
+
 # invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+
--- flaskdb.py
+++ flaskdb.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+# class City(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(50), nullable=False)
